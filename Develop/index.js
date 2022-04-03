45b33ce9a48399409cca08be95b0c9e0d9223482
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-03 15:54:51 -0500

    add generatemarkdown fuctionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,27 +1,27 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require("inquirer")
 const fs = require("fs")
 const generateMarkdown = require("./utils/generateMarkdown")
-/ TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [{
 
     // project name
     type: "input",
-    name: "#title",
+    name: "title",
     message: "Please enter your project name"
 },
     // description 
 {
     type: "input",
-    name: "##description",
+    name: "description",
     message: "Enter a description of your project"
 },
     // optional table of contents
-    .
+    
     // Usage steps on how to install
 {
     type: "input",
-    name: "##installation",
+    name: "installation",
     message: "What are the steps required to install your project?"
 },
 
@@ -33,22 +33,26 @@ const questions = [{
     // badges
     // features
     // Contributions
-]};
+];
 
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
     console.log(fileName)
     fs.writeFileSync(fileName, data)
 }
 
-// TODO: Create a function to initialize app
-function init() 
+// Create a function to initialize app
+function init() {
     inquirer
     .prompt(questions)
     .then((answers) =>{
     console.log(answers)
-})}
+    var readme = generateMarkdown(answers)
+    console.log(readme)
+    writeToFile("Readme.md", readme)
+    })
+}
 
 // Function call to initialize app
 init();
