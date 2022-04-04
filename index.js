8b91e537de9f9618ca85ec3c4d8051e7347902f0
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-03 21:54:13 -0500

    tested and previewed code seems to be working well so far still need to add badges and license link

--- index.js
+++ index.js
@@ -1,85 +1,79 @@
 // Include packages needed for this application
-const inquirer = require("inquirer")
-const fs = require("fs")
+const inquirer = require("inquirer");
+const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 const Choice = require("inquirer/lib/objects/choice");
 // Create an array of questions for user input
-const questions = [{
+const questions = [
+    {
+        // project name
+        type: "input",
+        name: "title",
+        message: "Please enter your project name",
+    },
+    // description
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project",
+    },
 
-    // project name
-    type: "input",
-    name: "title",
-    message: "Please enter your project name"
-},
-// description 
-{
-    type: "input",
-    name: "description",
-    message: "Enter a description of your project"
-},
+    // Usage steps on how to install
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?",
+    },
 
-// Usage steps on how to install
-{
-    type: "input",
-    name: "installation",
-    message: "What are the steps required to install your project?"
-},
-
-// github username 
-{
-    type: "input",
+    // github username
+    {
+        type: "input",
         name: "username",
-        message: "What is your github user name?"
-},
+        message: "What is your github user name?",
+    },
 
-// license and badge 
-{
-    type: "checkbox",
-      name: "license",
-      message: "What license do you want to use?",
-      choices: [
-        "MIT",
-        "Apache-2.0",
-        "none"
-    ],
-},
+    // license and badge
+    {
+        type: "checkbox",
+        name: "license",
+        message: "What license do you want to use?",
+        choices: ["MIT", "Apache-2.0", "none"],
+    },
 
     // Contributions
     {
-    type: "input",
+        type: "input",
         name: "contributions",
-        message: "How can other developers contribute?"
-},
-// test 
-{
-    type: "input",
+        message: "How can other developers contribute?",
+    },
+    // test
+    {
+        type: "input",
         name: "test",
-        message: "Does your applications have tests? Please provide an example on how to run them here."
-},
-// email 
-{
-    type: "input",
-    name: "email",
-    message: "Please enter your email."
-}
+        message:
+            "Does your applications have tests? Please provide an example on how to run them here.",
+    },
+    // email
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email.",
+    },
 ];
-
 
 // Create a function to write README file
 function writeToFile(fileName, data) {
-    console.log(fileName)
-    fs.writeFileSync(fileName, data)
+    console.log(fileName);
+    fs.writeFileSync(fileName, data);
 }
 
 // Create a function to initialize app
 function init() {
-    inquirer
-        .prompt(questions)
-        .then((answers) => {
-            console.log(answers)
-            var readme = generateMarkdown(answers)            
-            writeToFile("Readme.md", readme)
-        })
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+        var readme = generateMarkdown(answers);
+        writeToFile("Readme.md", readme);
+    });
 }
 
 // Function call to initialize app
